faffd7823bf811f9eba3a020dcb6fb7d04b6686b
Nanjiifr <gaspardobert70@gmail.com>
2025-10-16 21:48:34 +0200

    simple bot v1

--- simple_bot.ml
+++ simple_bot.ml
@@ -42,65 +42,65 @@ let can_enter m newX newY used_spaces ally_pos ally_has_space =
      || (m.load = m.capacity && ally_has_space.(newX).(newY)))
 
 let playBestActions m map used_spaces ally_pos ally_has_space baseX baseY
-    enemyPos =
+    enemyPos hasEnemies hasResources =
   let x, y = (m.x, m.y)
   and load = m.load
   and capacity = m.capacity
   and attack = m.atk in
-  match capacity - load with
-  | n when n > 0 -> (
-      match attack with
-      | 5 ->
-          let dist = getEnemiesDist x y enemyPos in
-          let mDist =
-            Array.init (Array.length map) (fun i ->
-                let _, minValue, minIndex =
-                  Array.fold_left
-                    (fun (idx, cMin, cMinInd) d ->
-                      let cMin, cMinInd =
-                        if d < cMin then (d, idx) else (cMin, cMinInd)
-                      in
-                      (idx + 1, cMin, cMinInd))
-                    (0, max_int, 0) dist.(i)
-                in
-                (minValue, minIndex))
-          in
-          let _, (_, targetY), targetX =
-            Array.fold_left
-              (fun (idx, (cMin, cMinC), cMinInd) (d, ind) ->
-                let cMin, cMinC, cMinInd =
-                  if d < cMin then (d, ind, idx) else (cMin, cMinC, cMinInd)
-                in
-                (idx + 1, (cMin, cMinC), cMinInd))
-              (0, mDist.(0), 0)
-              mDist
-          in
-          let distToTarget =
-            [|
-              (calcDist (x + 1) y targetX targetY, (x + 1, y));
-              (calcDist (x - 1) y targetX targetY, (x - 1, y));
-              (calcDist x (y + 1) targetX targetY, (x, y + 1));
-              (calcDist x (y - 1) targetX targetY, (x, y - 1));
-            |]
-          in
-          Array.sort (fun (d1, _) (d2, _) -> d1 - d2) distToTarget;
-          let flag = ref true in
-          let i = ref 0 in
-          let actionPlayed = ref (x, y) in
-          let n = Array.length map in
-          while !i < 4 && !flag do
-            let _, (newX, newY) = distToTarget.(!i) in
-            if
-              0 <= newX && newX < n && 0 <= newY && newY < n
-              && can_enter m newX newY used_spaces ally_pos ally_has_space
-            then (
-              used_spaces.(newX).(newY) <- true;
-              flag := false;
-              actionPlayed := (newX, newY))
-            else incr i
-          done;
-          !actionPlayed
-      | _ -> (
+  match attack with
+  | n when (hasEnemies && n = 5) || not hasResources ->
+      let dist = getEnemiesDist x y enemyPos in
+      let mDist =
+        Array.init (Array.length map) (fun i ->
+            let _, minValue, minIndex =
+              Array.fold_left
+                (fun (idx, cMin, cMinInd) d ->
+                  let cMin, cMinInd =
+                    if d < cMin then (d, idx) else (cMin, cMinInd)
+                  in
+                  (idx + 1, cMin, cMinInd))
+                (0, max_int, 0) dist.(i)
+            in
+            (minValue, minIndex))
+      in
+      let _, (_, targetY), targetX =
+        Array.fold_left
+          (fun (idx, (cMin, cMinC), cMinInd) (d, ind) ->
+            let cMin, cMinC, cMinInd =
+              if d < cMin then (d, ind, idx) else (cMin, cMinC, cMinInd)
+            in
+            (idx + 1, (cMin, cMinC), cMinInd))
+          (0, mDist.(0), 0)
+          mDist
+      in
+      let distToTarget =
+        [|
+          (calcDist (x + 1) y targetX targetY, (x + 1, y));
+          (calcDist (x - 1) y targetX targetY, (x - 1, y));
+          (calcDist x (y + 1) targetX targetY, (x, y + 1));
+          (calcDist x (y - 1) targetX targetY, (x, y - 1));
+        |]
+      in
+      Array.sort (fun (d1, _) (d2, _) -> d1 - d2) distToTarget;
+      let flag = ref true in
+      let i = ref 0 in
+      let actionPlayed = ref (x, y) in
+      let n = Array.length map in
+      while !i < 4 && !flag do
+        let _, (newX, newY) = distToTarget.(!i) in
+        if
+          0 <= newX && newX < n && 0 <= newY && newY < n
+          && can_enter m newX newY used_spaces ally_pos ally_has_space
+        then (
+          used_spaces.(newX).(newY) <- true;
+          flag := false;
+          actionPlayed := (newX, newY))
+        else incr i
+      done;
+      !actionPlayed
+  | _ -> (
+      match capacity - load with
+      | n when n > 0 -> (
           match map.(x).(y) with
           | "R", j, _ when j > 0 -> (x, y)
           | _ ->
@@ -152,35 +152,35 @@ let playBestActions m map used_spaces ally_pos ally_has_space baseX baseY
                   actionPlayed := (newX, newY))
                 else incr i
               done;
-              !actionPlayed))
-  | _ ->
-      let targetX, targetY = (baseX, baseY) in
-      let distToTarget =
-        [|
-          (calcDist (x + 1) y targetX targetY, (x + 1, y));
-          (calcDist (x - 1) y targetX targetY, (x - 1, y));
-          (calcDist x (y + 1) targetX targetY, (x, y + 1));
-          (calcDist x (y - 1) targetX targetY, (x, y - 1));
-        |]
-      in
-      Array.sort (fun (d1, _) (d2, _) -> d1 - d2) distToTarget;
-      let flag = ref true in
-      let i = ref 0 in
-      let actionPlayed = ref (x, y) in
-      let n = Array.length map in
-      while !i < 4 && !flag do
-        let _, (newX, newY) = distToTarget.(!i) in
-        if
-          0 <= newX && newX < n && 0 <= newY && newY < n
-          && can_enter m newX newY used_spaces ally_pos ally_has_space
-        then (
-          used_spaces.(newX).(newY) <- true;
-          (* réserver la destination *)
-          flag := false;
-          actionPlayed := (newX, newY))
-        else incr i
-      done;
-      !actionPlayed
+              !actionPlayed)
+      | _ ->
+          let targetX, targetY = (baseX, baseY) in
+          let distToTarget =
+            [|
+              (calcDist (x + 1) y targetX targetY, (x + 1, y));
+              (calcDist (x - 1) y targetX targetY, (x - 1, y));
+              (calcDist x (y + 1) targetX targetY, (x, y + 1));
+              (calcDist x (y - 1) targetX targetY, (x, y - 1));
+            |]
+          in
+          Array.sort (fun (d1, _) (d2, _) -> d1 - d2) distToTarget;
+          let flag = ref true in
+          let i = ref 0 in
+          let actionPlayed = ref (x, y) in
+          let n = Array.length map in
+          while !i < 4 && !flag do
+            let _, (newX, newY) = distToTarget.(!i) in
+            if
+              0 <= newX && newX < n && 0 <= newY && newY < n
+              && can_enter m newX newY used_spaces ally_pos ally_has_space
+            then (
+              used_spaces.(newX).(newY) <- true;
+              (* réserver la destination *)
+              flag := false;
+              actionPlayed := (newX, newY))
+            else incr i
+          done;
+          !actionPlayed)
 
 let () =
   let ptr = open_in "mapData.txt" in
@@ -259,6 +259,38 @@ TYPE_VITESSE = 'P'
   Array.iter
     (fun m -> if m.owner <> myID then enemyPos.(m.x).(m.y) <- true)
     minions;
+  let hasEnemies =
+    Array.fold_left ( || ) false
+      (Array.map (fun v -> Array.fold_left ( || ) false v) enemyPos)
+  in
+
+  let resourcesPos = Array.make_matrix mapSize mapSize false in
+  Array.iteri
+    (fun i v ->
+      Array.iteri
+        (fun j v' ->
+          match v' with
+          | "R", k, _ when k > 0 -> resourcesPos.(i).(j) <- true
+          | _ -> ())
+        v)
+    map;
+  let hasResources =
+    Array.fold_left ( || ) false
+      (Array.map (fun v -> Array.fold_left ( || ) false v) resourcesPos)
+  in
+
+  let minionCount =
+    Array.fold_left
+      (fun acc m -> if m.owner = myID then acc + 1 else acc)
+      0 minions
+  in
+
+  let forrorCount =
+    Array.fold_left
+      (fun acc m -> if m.owner = myID && m.capacity = 18 then acc + 1 else acc)
+      0 minions
+  in
+  let attackerCount = minionCount - forrorCount in
 
   let ally_pos = Array.make_matrix mapSize mapSize false in
   let ally_has_space = Array.make_matrix mapSize mapSize false in
@@ -271,15 +303,18 @@ TYPE_VITESSE = 'P'
 
   Array.iter
     (fun minion ->
-      if minion.owner = myID then
+      if minion.owner = myID then (
+        let temp = usedSpaces.(baseX).(baseY) in
+        if minion.load = 0 then usedSpaces.(baseX).(baseY) <- true;
         let newX, newY =
           playBestActions minion map usedSpaces ally_pos ally_has_space baseX
-            baseY enemyPos
+            baseY enemyPos hasEnemies hasResources
         in
-        Printf.fprintf ptrOut "%d %d %d %d\n" minion.x minion.y newX newY)
+        usedSpaces.(baseX).(baseY) <- temp;
+        Printf.fprintf ptrOut "%d %d %d %d\n" minion.x minion.y newX newY))
     minions;
 
-  if curTurn < 100 || curTurn mod 10 == 0 then (
+  if forrorCount < 15 && (curTurn <= 100 || attackerCount > 15) then (
     if myResources >= 10 then Printf.fprintf ptrOut "CREATE 1 9 0\n")
   else if myResources >= 15 then Printf.fprintf ptrOut "CREATE 5 5 5\n";
 
